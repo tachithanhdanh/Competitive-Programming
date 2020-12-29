6c286d8b9a3d824d8debc3ad254352576e6cae67
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2020-12-29 16:17:40 +0700

    Add files via upload

---
+++ USACO/Bronze/introduction_to_graph_theory/factory_editorial.cpp
@@ -0,0 +1,129 @@
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <climits>
+#include <cfloat>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <list>
+#include <array>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <iterator>
+#include <ctime>
+#include <tuple>
+#include <numeric>
+ 
+using namespace std;
+
+using ll = long long;
+using si = short int;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+
+#define ar array
+#define endl "\n" 
+#define hash_set unordered_set 
+#define hash_map unordered_map
+
+//vector
+#define vt vector
+#define all(x) begin(x), end(x) 
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define rsz resize
+#define pb push_back
+#define pf push_front
+#define ft front()
+#define bk back()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+//pairs and tuples
+#define f first
+#define s second
+#define mp make_pair
+#define mtp make_tuple
+
+void setIO(string name =""){
+//Compile with those flags and to get the fastest performance
+//g++.exe -std=c++11 -DLOCAL ${file} -o ${file_base_name}.exe -Wl,--stack,268435456 -O2 && ${file_base_name}.exe
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	if (name.size()){
+		freopen((name+".in").c_str(),"r",stdin);
+		freopen((name+".out").c_str(),"w",stdout);
+	}
+	else {
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);  
+		freopen("output.txt", "w", stdout); 
+		freopen("error.txt", "w", stderr);
+		#endif 
+	}
+	return;
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=1e6+10;
+
+//variables used for the current problem
+int n,a,b,outgoing[101],ingoing[101],ans=-1;
+
+void solve(){ 
+	//http://www.usaco.org/current/data/sol_factory_bronze_open19.html
+	//http://www.usaco.org/index.php?page=viewproblem2&cpid=940
+	cin >> n;
+	for (int i=1;i<n;++i){
+		cin >> a >> b;
+		++outgoing[a]; ++ingoing[b];
+	}
+  	for (int i=1; i<=N; i++) {
+   		if (outgoing[i]==0 && ans != -1 ) { ans = -1; break; } // found two sinks -- bad!
+    	if (outgoing[i]==0) ans = i;  // found first sink; remember it
+  	}
+	cout << ans << endl;
+}
+// you should actually read the stuff below
+// read!read!read!read!read!read!read!read!read!
+// ll vs. int!
+ 
+/* stuff you should look for
+* int overflow, array bounds
+* special cases (n=1?)
+* do smth instead of nothing and stay organized
+* WRITE STUFF DOWN
+* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+	setIO("factory");
+	//cout.tie(NULL);
+	/*
+	cin >> t;
+	for (int i=1;i<=t;++i){
+		//cout << "Test " << i << endl; 
+		solve();
+	}
+	*/
+	solve();
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl; 
+	return 0;
+}
