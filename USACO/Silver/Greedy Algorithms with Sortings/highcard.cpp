056f1de7ab7cd7aae06d3d4702df0fd6265857b9
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-03 08:41:42 +0700

    Add files via upload

---
+++ USACO/Silver/Greedy Algorithms with Sortings/highcard.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using si = short int;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+
+#define endl "\n"
+//Make sure to define this
+//in case you accidentally use endl instead of "\n" to make a new line
+
+#define hash_set unordered_set
+#define hash_map unordered_map
+
+//vector
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define rsz resize
+#define pb push_back
+#define pf push_front
+#define ft front()
+#define bk back()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+//pairs and tuples
+#define f first
+#define s second
+#define mp make_pair
+#define mtp make_tuple
+
+void setIO(string name =""){
+//Compile with those flags and to get the fastest performance
+//g++.exe -std=c++11 -DLOCAL ${file} -o ${file_base_name}.exe -Wl,--stack,268435456 -O2 && ${file_base_name}.exe
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	if (name.size()){
+		freopen((name+".in").c_str(),"r",stdin);
+		freopen((name+".out").c_str(),"w",stdout);
+	}
+	else {
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);
+		#endif
+	}
+	return;
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=1e5+10;
+
+//variables used for the current problem
+int n,a[maxn],ans; set<int> card; bool cnt[maxn*2];
+
+void solve(){
+    cin >>n;
+    for (int i=0; i<n; ++i){
+        cin >> a[i]; cnt[a[i]]=true;
+    }
+    for (int i=1;i<=2*n;++i){
+        if (cnt[i]==false) card.ins(i);
+    }
+    for (int i=0;i<n;++i){
+        auto it = card.ub(a[i]);
+        if (it==card.end()) it=card.begin();
+        ans+= *it > a[i];
+        card.erase(it);
+    }
+    cout << ans << endl;
+}
+// you should actually read the stuff below
+// read!read!read!read!read!read!read!read!read!
+// ll vs. int!
+//DELETE ALL THE DEBUG LINES BEFORE SUBMITTING PLEASE
+/* stuff you should look for
+* int overflow, array bounds
+* special cases (n=1?)
+* do smth instead of nothing and stay organized
+* WRITE STUFF DOWN
+* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+	setIO("highcard");
+	//cout.tie(NULL);
+	/*
+	cin >> t;
+	for (int i=1;i<=t;++i){
+		//cout << "Test " << i << endl;
+		solve();
+	}
+	*/
+	solve();
+	cerr<< setprecision(5) << "time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl;
+	return 0;
+}
