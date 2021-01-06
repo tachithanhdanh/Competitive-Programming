74546c6b8596e3855bd07b95ce573075c7633f7d
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-06 20:03:21 +0700

    Add files via upload

---
+++ USACO/Silver/Binary Search on Anwers/convention.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using si = short int;
+using uint = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+
+#define endl "\n"
+//Make sure to define this
+//in case you accidentally use endl instead of "\n" to make a new line
+
+#define hash_set unordered_set
+#define hash_map unordered_map
+
+//vector
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define rsz resize
+#define pb push_back
+#define pf push_front
+#define ft front()
+#define bk back()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+//pairs and tuples
+#define f first
+#define s second
+#define mp make_pair
+#define mtp make_tuple
+
+void setIO(string name =""){
+//Compile with those flags and to get the fastest performance
+//g++.exe -std=c++11 -DLOCAL ${file} -o ${file_base_name}.exe -Wl,--stack,268435456 -O2 && ${file_base_name}.exe
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	if (name.size()){
+		freopen((name+".in").c_str(),"r",stdin);
+		freopen((name+".out").c_str(),"w",stdout);
+	}
+	else {
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+		freopen("error.txt", "w", stderr);
+		#endif
+	}
+	return;
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=1e5+10;
+const ll INF=1e18;
+
+//variables used for the current problem
+int n,m,c,a[maxn],lo,mid,hi,ans;
+
+void solve(){
+	cin >> n >> m >> c;
+	for (int i=0;i<n;++i){
+		cin >> a[i];
+	}
+	sort(a,a+n);
+	auto check = [&](const int t){
+		int cows=0,buses=0,mn=0;
+		for (int i=0;i<n;++i){
+			if (cows==0){
+				++buses; ++cows;
+				mn= a[i];
+			}
+			else{
+				if (cows<c && a[i]-mn<=t){
+					++cows;
+				}
+				else{
+					cows=1; ++buses; mn = a[i];
+				}
+			}
+		}
+		return (buses<=m);
+	};
+	lo = 0, hi = mod;
+	while (lo <= hi){
+		mid = (lo + hi)/2;
+		if (check(mid)){
+			hi = mid - 1;
+			ans = mid;
+		}
+		else{
+			lo = mid + 1;
+		}
+	}
+	cout << ans << endl;
+}
+// you should actually read the stuff below
+// read!read!read!read!read!read!read!read!read!
+// ll vs. int!
+//DELETE ALL THE DEBUG LINES BEFORE SUBMITTING PLEASE
+/* stuff you should look for
+* int overflow, array bounds
+* special cases (n=1?)
+* do smth instead of nothing and stay organized
+* WRITE STUFF DOWN
+* DON'T GET STUCK ON ONE APPROACH
+*/
+int main(){
+	setIO("convention");
+	//cout.tie(NULL);
+	solve();
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl;
+	return 0;
+}
