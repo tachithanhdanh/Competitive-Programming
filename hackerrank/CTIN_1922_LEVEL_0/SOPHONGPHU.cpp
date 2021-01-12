d7d102fdab87fcdac0ac5db50101b58123a25e2d
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-12 20:57:57 +0700

    Add files via upload

---
+++ hackerrank/CTIN_1922_LEVEL_0/SOPHONGPHU.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a : x)
+
+void setIO(string name = "") { 
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+	else{
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);  
+		freopen("output.txt", "w", stdout); 
+		freopen("error.txt", "w", stderr);
+		#endif 
+	}
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=2e5+10;
+
+//variables used for the current problem
+int n,a,sum; vi ans;
+
+void solve(){
+	cin >> n;
+	F0R(i,n){
+		cin >> a;
+		sum=0;
+		for (int j=2;j*j<=a;++j){
+			if (a%j==0){
+				sum += j;
+				if (j*j!=a) sum+=(a/j);
+			}
+		}
+		if (sum>=a) ans.pb(a);
+	}
+	if (sz(ans)) trav(a,ans) cout << a << endl;
+	else cout << -1 << endl;
+	return;
+}
+
+int main() {
+	setIO();
+	solve();
+	return 0;
+}
