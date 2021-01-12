fdb078b599c0f25f29a6a5980cde8b39f49ad83b
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-12 21:50:33 +0700

    Add files via upload

---
+++ hackerrank/CTIN_1922_LEVEL_0/NHIPHAN.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a : x)
+
+void setIO(string name = "") { 
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+	else{
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);  
+		freopen("output.txt", "w", stdout); 
+		freopen("error.txt", "w", stderr);
+		#endif 
+	}
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=2e5+10;
+
+//variables used for the current problem
+int n,cnt,i;
+
+void solve(){
+	cin >> n;
+	while ((1<<i) <=n) ++i;
+	for (--i;(1<<i) >= 0;--i){
+		cout << (n & (1<<i) ? ++cnt,1 : 0);
+	}
+	cout << endl << cnt;
+	return;
+}
+
+int main() {
+	setIO();
+	solve();
+	return 0;
+}
