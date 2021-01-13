6ae929a4add007557b8c290f643e7d58f8fcf3ee
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-13 21:59:41 +0700

    Add files via upload

---
+++ hackerrank/CTIN_1922_LEVEL_0/SNN.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a : x)
+
+void setIO(string name = "") { 
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+	else{
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);  
+		freopen("output.txt", "w", stdout); 
+		freopen("error.txt", "w", stderr);
+		#endif 
+	}
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=1e3+10;
+const ll INF = 1e18;
+
+//variables used for the current problem
+int n; vi a;
+
+void solve(){
+	cin >> n;
+	a.rsz(n);
+	trav(b,a) cin >> b;
+	sort(all(a));
+	if (*a.begin()) cout << 0;
+	else{
+		FOR(i,1,n){
+			if (a[i]!=a[i-1]+1 && a[i]!=a[i-1]){
+				cout << a[i-1]+1; return;
+			}
+		}
+		cout << a[n-1]+1;
+	}
+	return;
+}
+
+int main() {
+	setIO();
+	solve();
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl;
+	return 0;
+}
