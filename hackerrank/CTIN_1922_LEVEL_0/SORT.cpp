7ba7787e28c4cfe8dee67a4e781c51c72e59a450
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-14 20:17:42 +0700

    Add files via upload

---
+++ hackerrank/CTIN_1922_LEVEL_0/SORT.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a : x)
+
+void setIO(string name = "") { 
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+	else{
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);  
+		freopen("output.txt", "w", stdout); 
+		freopen("error.txt", "w", stderr);
+		#endif 
+	}
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=2e5+10;
+const ll INF = 1e18;
+
+//variables used for the current problem
+int n; vi ans[3];
+
+void solve(){
+	cin >> n;
+	while (cin >> n){
+		if (n==0) ans[1].pb(n);
+		else if (n>0) ans[2].pb(n);
+		else ans[0].pb(n);
+	}
+	//sort(all(ans[2])); sort(all(ans[0]),greater<int>());
+	trav(v,ans) trav(a,v) cout << a << " ";
+	return;
+}
+
+int main() {
+	setIO();
+	solve();
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl;
+	return 0;
+}
