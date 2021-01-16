343912c6993e38bbc009df268acf40fcffd1a80a
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-16 15:29:40 +0700

    Add files via upload

---
+++ hackerrank/CTIN_1922_LEVEL_0/CHUKY.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a : x)
+
+void setIO(string name = "") { 
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+	else{
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);  
+		freopen("output.txt", "w", stdout); 
+		freopen("error.txt", "w", stderr);
+		#endif 
+	}
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int MOD=1e9+7;
+const int maxn=1e5+10;
+const ll INF = 1e18;
+
+//variables used for the current problem
+int a,b,mn=MOD,mx,n,cnt;
+
+void solve(){
+	cin >> a >> b;
+	FOR(i,a,b+1){
+		n = i;
+		cnt=1;
+		while (n!=1){
+			if (n&1) n=n*3+1;
+			else n/=2;
+			++cnt;
+		}
+		mx = max(cnt,mx);
+		mn = min(cnt,mn);
+	}
+	cout << mx + mn << endl;
+	return;
+}
+
+int main() {
+	setIO();
+	solve();
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl;
+	return 0;
+}
