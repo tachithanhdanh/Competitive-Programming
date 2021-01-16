355c3851a0559a54f5b3ad5f6afa5297e2f95fb2
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-01-16 14:12:02 +0700

    Add files via upload

---
+++ hackerrank/CTIN_1922_LEVEL_0/TPB-GAMESHOW.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+	else{
+		#ifdef LOCAL
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+		freopen("error.txt", "w", stderr);
+		#endif
+	}
+}
+
+//constant initialization
+const string yes="YES",no="NO";
+const int mod=1e9+7;
+const int maxn=2e5+10;
+
+//variables used for the current problem
+int n,k,a,b; vi v;
+
+void solve(){
+	cin >> n >> k;
+	v.rsz(n);
+	iota(all(v),1);
+	while (k--){
+        cin >> a >> b;
+        if (a>b) swap(a,b);
+        reverse(v.begin()+a-1,v.begin()+b);
+	}
+	trav(c,v) cout << c << " ";
+	return;
+}
+
+int main() {
+	setIO();
+	solve();
+	return 0;
+}
