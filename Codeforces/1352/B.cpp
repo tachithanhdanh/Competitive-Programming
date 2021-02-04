cbf35f06ae2c67c1c7cc90c0b8869e8a2486c27e
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-04 10:28:37 +0700

    Add files via upload

---
+++ Codeforces/1352/B.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vector
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define each(a,x) for (auto& a: x)
+
+//constant initialization
+const string yes="YES\n",no="NO\n";
+const int MOD=1e9+7; //998244353
+const ll INF = 1e18; //Not too close to LLONG_MAX
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    #ifdef LOCAL
+        freopen("input.txt","r",stdin);
+    #endif // LOCAL
+    int T; cin >> T;
+    while (T--) {
+        int N, K; cin >> N >> K;
+        int N1 = N - (K-1);
+        int N2 = N - 2*(K-1);
+        if (N1>0 && N1&1) {
+            cout << yes;
+            F0R(i,K-1) cout << 1 << " ";
+            cout << N1 << endl;
+        }
+        else if (N2>0 && !(N2&1)) {
+            cout << yes;
+            F0R(i,K-1) cout << "2 ";
+            cout << N2 << endl;
+        }
+        else cout << no;
+    }
+	return 0;
+}
