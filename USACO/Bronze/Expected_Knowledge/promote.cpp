a2f80285dd204dee63afef565a4edb6bfe219ae8
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-04 13:44:06 +0700

    Update promote.cpp

--- USACO/Bronze/Expected_Knowledge/promote.cpp
+++ USACO/Bronze/Expected_Knowledge/promote.cpp
@@ -1,62 +1,35 @@
 #include <iostream>
-#include <iomanip>
 #include <cstdio>
-#include <cmath>
-#include <cctype>
-#include <climits>
-#include <cfloat>
-#include <cstring>
-#include <fstream>
-#include <vector>
-#include <iterator>
-#include <typeinfo>
-#include <set>
-#include <list>
-#include <map>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <unordered_set>
-#include <unordered_map>
-#include <utility>
-
-#define endl "\n"
-#define pub push_back
-#define pob pop_back
-#define ins insert
-#define mp make_pair
-
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef short int si;
-
-int a,b,c,d,e,f,g,h;
-
-void solve(){ //http://www.usaco.org/index.php?page=viewproblem2&cpid=591
-    cin >> a >> b >> c >> d >> e >> f >> g >> h;
-    cout << h-g+f-e+d-c << endl << h-g+f-e << endl << h-g;
-    return;
-}
-
 int main(){
-    //Read input from STDIN, then print the output to STDOUT.
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    freopen("promote.in","r",stdin);
-    freopen("promote.out","w",stdout);
-    //cin >> t; 
-    //while (t--) {
-        //cout << "Case #" << ++cnt << ": "; 
-        solve();
-    //}
-    return 0;
-}
+	// initialize file I/O
+	freopen("promote.in","r",stdin);
+	freopen("promote.out","w",stdout);
 
+	// read in counts for bronze
+	int bronzeBefore, bronzeAfter;
+	cin >> bronzeBefore >> bronzeAfter;
+		
+	// read in counts for silver
+	int silverBefore, silverAfter;
+	cin >> silverBefore >> silverAfter;
+
+	// read in counts for gold
+	int goldBefore, goldAfter;
+	cin >> goldBefore >> goldAfter;
+	
+	// read in counts for platinum
+	int platinumBefore, platinumAfter;
+	cin >> platinumBefore >> platinumAfter;
+		
+	// do the computations
+	int goldToPlatinum = platinumAfter - platinumBefore;
+	int silverToGold = goldAfter - goldBefore + platinumAfter - platinumBefore;
+	int bronzeToSilver = silverAfter - silverBefore + goldAfter - goldBefore + platinumAfter - platinumBefore;
+		
+	// print the answers
+	cout << bronzeToSilver << "\n" << silverToGold << "\n" << goldToPlatinum;
+		
+	return 0;
+}
