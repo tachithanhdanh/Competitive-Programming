1939b002e6fcca188885c2720fcbff57f9c7d402
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-04 14:17:16 +0700

    Update word.cpp

--- USACO/Bronze/Expected_Knowledge/word.cpp
+++ USACO/Bronze/Expected_Knowledge/word.cpp
@@ -1,75 +1,40 @@
 #include <iostream>
-#include <iomanip>
 #include <cstdio>
-#include <cmath>
-#include <cctype>
-#include <climits>
-#include <cfloat>
-#include <cstring>
-#include <fstream>
-#include <vector>
-#include <iterator>
-#include <typeinfo>
-#include <set>
-#include <list>
-#include <map>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <algorithm>
 #include <string>
-#include <sstream>
-#include <unordered_set>
-#include <unordered_map>
-#include <utility>
-
-#define endl "\n"
-#define pub push_back
-#define pob pop_back
-#define ins insert
-#define mp make_pair
-
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef short int si;
-
-si n,k,ch; string s; 
-
-void solve(){
-    //http://www.usaco.org/index.php?page=viewproblem2&cpid=987
-    cin >> n >> k >> s;
-    cout << s; ch+=s.size();
-    while (--n){
-        cin >> s;
-        ch+=s.size();
-        if (ch<=k) {
-            cout << " ";
-            cout << s;
-        }
-        else {
-            cout << endl; 
-            cout << s; ch=s.size();
-        } 
-    }
-    return;
-}
-
 int main(){
-    //Read input from STDIN, then print the output to STDOUT.
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    freopen("word.in","r",stdin);
-    freopen("word.out","w",stdout);
-    //cin >> t; 
-    //while (t--) {
-        //cout << "Case #" << ++cnt << ": "; 
-        solve();
-    //}
-    return 0;
-}
+	ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
+	freopen("word.in","r",stdin);
+	freopen("word.out","w",stdout);
 
+	//Read N and K.
+	int N, K;
+	cin >> N >> K;
+
+	//Read the first word and print it immediately.
+	string S;
+	cin >> S;
+	cout << S;
+
+	//This variable is used for storing the number of characters
+	//on the current line but NOT including the spaces characters.
+	int Word_Length = 0;
+	Word_Length += S.length();
+
+	//Since there are N-1 words left, we only loop up to N-1.
+	for (int i = 0; i < N - 1; ++i){
+        cin >> S; //Read the next word.
+		//Calculate the new length if we put the new word into the current line.
+        Word_Length += S.length(); 
+        if (Word_Length <= K) { //If it still satisfies the constraint then print that word.
+            cout << " ";
+            cout << S;
+        }
+        else { //Else we create a new line and print the word on that line.
+            cout << "\n" << s;
+			Word_Length = S.length();
+        } 
+    }	
+	return 0;
+}
