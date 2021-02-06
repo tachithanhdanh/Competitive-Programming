91b9663cf6c446d6ea1fa16521efa8948bf7b730
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-06 08:25:08 +0700

    Update square.cpp

--- USACO/Contests/USACO Solutions/2016-17/Dec 2016/Bronze/square.cpp
+++ USACO/Contests/USACO Solutions/2016-17/Dec 2016/Bronze/square.cpp
@@ -5,18 +5,14 @@ using namespace std;
 
 int x[4],y[4],a,b,c,d,ans;
 
-void solve(){
+int main(){
+	freopen("square.in","r",stdin);
+	freopen("square.out","w",stdout);
 	for (int i=0;i<4;++i){
 		cin >> x[i] >> y[i];
 	}
 	a=min(x[0],x[2]); b=min(y[0],y[2]); c=max(x[1],x[3]); d=max(y[1],y[3]);
 	ans=max((c-a)*(c-a),(d-b)*(d-b));
 	cout << ans;
-}
-
-int main(){
-	freopen("square.in","r",stdin);
-	freopen("square.out","w",stdout);
-	solve();
 	return 0;
 }
