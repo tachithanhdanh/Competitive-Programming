9d35a0115529500e174edb20addb72b603d5c284
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-06 08:01:03 +0700

    Update badmilk.cpp

--- USACO/Contests/USACO Solutions/2015-16/Dec 2015/Bronze/badmilk.cpp
+++ USACO/Contests/USACO Solutions/2015-16/Dec 2015/Bronze/badmilk.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 //vector
-#define vt vector
 #define all(x) begin(x), end(x) 
 #define sz(x) (int)(x).size()
 #define rsz resize
@@ -13,34 +12,17 @@ using namespace std;
 #define s second
 #define mp make_pair
 
-void setIO(string name =""){
-//Compile with those flags and to get the fastest performance
-//g++.exe -std=c++11 -DLOCAL ${file} -o ${file_base_name}.exe -Wl,--stack,268435456 -O2 && ${file_base_name}.exe
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	if (name.size()){
-		freopen((name+".in").c_str(),"r",stdin);
-		freopen((name+".out").c_str(),"w",stdout);
-	}
-	else {
-		#ifdef LOCAL
-		freopen("input.txt", "r", stdin);  
-		freopen("output.txt", "w", stdout); 
-		freopen("error.txt", "w", stderr);
-		#endif 
-	}
-	return;
-}
-
-//constant initialization
-const string yes="YES",no="NO";
-const int mod=1e9+7;
-
 //variables used for the current problem
-int N,M,D,S,m,p,t,sick[51],possible[51],cnt,ans,badmilk; vt<vt<pair<int,int>>> v;
+int N,M,D,S,m,p,t;
+int sick[51],possible[51],cnt,ans,badmilk;
+vector<vector<pair<int,int>>> v;
 bool flag,milkflag[51];
 
-void solve(){
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("badmilk.in","r",stdin);
+	freopen("badmilk.out","w",stdout);
 	cin >> N >> M >> D >> S;
 	v.rsz(N+1);
 	for (int i=0;i<D;++i){
@@ -84,10 +66,5 @@ void solve(){
 	}
 	ans=max(ans,cnt);
 	cout << S+ans;
-}
-
-int main(){
-	setIO("badmilk");
-	solve();
 	return 0;
 }
