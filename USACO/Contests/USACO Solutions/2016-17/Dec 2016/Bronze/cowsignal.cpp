be8773730d6d0ff79543618517f5d873b3230118
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-06 08:24:34 +0700

    Update cowsignal.cpp

--- USACO/Contests/USACO Solutions/2016-17/Dec 2016/Bronze/cowsignal.cpp
+++ USACO/Contests/USACO Solutions/2016-17/Dec 2016/Bronze/cowsignal.cpp
@@ -8,15 +8,15 @@ int main(){
 	freopen("cowsignal.out","w",stdout);
 	int m,n,k; char c[10][10];
 	cin >> m >> n >> k;
-	for (si i=0;i<m;++i){
-		for (si j=0;j<n;++j){
+	for (int i=0;i<m;++i){
+		for (int j=0;j<n;++j){
 			cin >> c[i][j];
 		}
 	}
-	for (si i=0;i<m;++i){
-		for (si o=0;o<k;++o){
-			for(si j=0;j<n;++j){
-				for (si p=0;p<k;++p){
+	for (int i=0;i<m;++i){
+		for (int o=0;o<k;++o){
+			for(int j=0;j<n;++j){
+				for (int p=0;p<k;++p){
 					cout << c[i][j];
 				}
 			}
