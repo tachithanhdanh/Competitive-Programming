ef39152f97b4429217da1781f68037a51cf92301
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-06 08:03:00 +0700

    Update paint.cpp

--- USACO/Contests/USACO Solutions/2015-16/Dec 2015/Bronze/paint.cpp
+++ USACO/Contests/USACO Solutions/2015-16/Dec 2015/Bronze/paint.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
-
 using namespace std;
-
-using si = short int;
-
-si a,b,c,d;
-//http://www.usaco.org/index.php?page=viewproblem2&cpid=567
-
-void solve(){
-	cin >> a >> b >> c >> d;
-	if (b<c || d<a) cout << (b-a)+(d-c);
-	else cout << max(b,d)-min(a,c);
-	return;
-}
 
 int main(){
 	//Read input from STDIN, then print the output to STDOUT.
@@ -22,7 +9,10 @@ int main(){
 	cin.tie(NULL); 
 	freopen("paint.in","r",stdin);
 	freopen("paint.out","w",stdout);
-	solve();
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (b < c ||  d < a) cout << (b - a) + (d - c);
+	else cout << max(b,d)-min(a,c);
 	return 0;
 }
 
