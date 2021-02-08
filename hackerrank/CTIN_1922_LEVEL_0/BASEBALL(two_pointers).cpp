6a705bfd8b6ab3089ab9214ec7fe8c00ec97bcf9
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-02-08 18:36:44 +0700

    Update BASEBALL(two_pointers).cpp

--- hackerrank/CTIN_1922_LEVEL_0/BASEBALL(two_pointers).cpp
+++ hackerrank/CTIN_1922_LEVEL_0/BASEBALL(two_pointers).cpp
@@ -1,57 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using si = short int;
-using uint = unsigned int;
-using ull = unsigned long long;
-using ld = long double;
-
-#define ar array
-#define endl "\n" 
-#define hash_set unordered_set 
-#define hash_map unordered_map
-
-//vector
-#define vt vector
-#define all(x) begin(x), end(x) 
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
-#define rsz resize
-#define pb push_back
-#define pf push_front
-#define ft front()
-#define bk back()
-#define ins insert
-#define lb lower_bound
-#define ub upper_bound
-
-//pairs and tuples
-#define f first
-#define s second
-#define mp make_pair
-#define mtp make_tuple
-
-void setIO(string name =""){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	if (name.size()){
-		freopen((name+".in").c_str(),"r",stdin);
-		freopen((name+".out").c_str(),"w",stdout);
-	}
-	else {
-		#ifdef LOCAL
-		freopen("input.txt", "r", stdin);  
-		freopen("output.txt", "w", stdout); 
-		freopen("error.txt", "w", stderr);
-		#endif 
-	}
-	return;
-}
-
 int n,a[1050],l,r,ans;
 
-void solve(){
+int main(){
+	//http://www.usaco.org/index.php?page=viewproblem2&cpid=359
+	cin.tie(0)->sync_with_stdio(0);
+	freopen("baseball.in","r",stdin);
+	freopen("baseball.out","w",stdout);
 	cin >> n;
 	for (int i=0;i<n;++i){
 		cin >> a[i];
@@ -71,24 +27,6 @@ void solve(){
 			}
 		}
 	}
-	cout << ans << endl;
-	return;
-}
-// you should actually read the stuff below
-// read!read!read!read!read!read!read!read!read!
-// ll vs. int!
- 
-/* stuff you should look for
-* int overflow, array bounds
-* special cases (n=1?)
-* do smth instead of nothing and stay organized
-* WRITE STUFF DOWN
-* DON'T GET STUCK ON ONE APPROACH
-*/
-int main(){
-	//http://www.usaco.org/index.php?page=viewproblem2&cpid=359
-	setIO();
-	solve();
-	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl; 
+	cout << ans << '\n';
 	return 0;
 }
