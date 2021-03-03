42852fc76e636e00697d11c01d718bcc5da84706
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-03-03 21:11:14 +0700

    Update NYTRAVEL.cpp

--- offline contest/DE_LOC_TUYEN_3_2021/NYTRAVEL.cpp
+++ offline contest/DE_LOC_TUYEN_3_2021/NYTRAVEL.cpp
@@ -1,59 +1,13 @@
-#include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <ciso646>
-#include <climits>
-#include <clocale>
-#include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
+#include <bits/stdc++.h> // see /general/running-code-locally
 using namespace std;
 
 #define endl "\n"
-//for k i j bruh
+
 using ll = long long;
 
 //vectors
 using vi = vector<int>;
-#define all(x) (x).begin(), (x).end()
+#define all(x) begin(x), end(x)
 #define sz(x) int((x).size())
 #define pb push_back
 
