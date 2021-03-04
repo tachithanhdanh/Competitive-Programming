9fb033b5a7e83ec44b0c68e672f14e19d1798a6f
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-03-04 20:18:23 +0700

    Update Template_CodeBlocks.cpp

--- Templates/Template_CodeBlocks.cpp
+++ Templates/Template_CodeBlocks.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // see /general/running-code-locally
 using namespace std;
 
 #define endl "\n"
@@ -17,18 +17,18 @@ using pi = pair<int,int>;
 #define s second
 #define mp make_pair
 
-void setIO (string name = "") {
-	cin.tie(0)->sync_with_stdio(0);
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
 	if (sz(name)) {
-		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".in").c_str(), "r", stdin); // see /general/io
 		freopen((name+".out").c_str(), "w", stdout);
 	}
 }
 
 //constant initialization
-const string yes="YES",no="NO";
-const int MOD=1e9+7; //998244353
-const int maxn=2e6+10;
+const string yes="YES\n",no="NO\n";
+const int MOD = 1e9+7; //998244353
+const int MX = 2e6+10;
 const ll INF = 1e18; //Not too close to LLONG_MAX
 
 //variables used for the current problem
