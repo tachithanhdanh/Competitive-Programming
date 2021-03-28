af8ddb3f34e3932f03d534c42804ce36aa0373f7
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-03-28 08:04:42 +0700

    Update badmilk.cpp

--- USACO/Bronze/basic_complete_search/badmilk.cpp
+++ USACO/Bronze/basic_complete_search/badmilk.cpp
@@ -1,65 +1,21 @@
-#include <sstream>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <climits>
-#include <cfloat>
-#include <string>
-#include <algorithm>
-#include <functional>
-#include <vector>
-#include <list>
-#include <array>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <unordered_set>
-#include <unordered_map>
-#include <utility>
-#include <iterator>
-#include <ctime>
-#include <tuple>
-#include <numeric>
- 
+#include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
-using si = short int;
-using uint = unsigned int;
-using ull = unsigned long long;
-using ld = long double;
 
-#define ar array
 #define endl "\n" 
-#define hash_set unordered_set 
-#define hash_map unordered_map
 
 //vector
-#define vt vector
+using vi = vector<int>;
 #define all(x) begin(x), end(x) 
-#define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
-#define rsz resize
 #define pb push_back
-#define pf push_front
-#define ft front()
-#define bk back()
-#define ins insert
-#define lb lower_bound
-#define ub upper_bound
 
-//pairs and tuples
+//pairs
+using pi = pair<int,int>;
 #define f first
 #define s second
 #define mp make_pair
-#define mtp make_tuple
 
 void setIO(string name =""){
 //Compile with those flags and to get the fastest performance
@@ -70,13 +26,6 @@ void setIO(string name =""){
 		freopen((name+".in").c_str(),"r",stdin);
 		freopen((name+".out").c_str(),"w",stdout);
 	}
-	else {
-		#ifdef LOCAL
-		freopen("input.txt", "r", stdin);  
-		freopen("output.txt", "w", stdout); 
-		freopen("error.txt", "w", stderr);
-		#endif 
-	}
 	return;
 }
 
@@ -85,12 +34,14 @@ const string yes="YES",no="NO";
 const int mod=1e9+7;
 
 //variables used for the current problem
-int N,M,D,S,m,p,t,sick[51],possible[51],cnt,ans,badmilk; vt<vt<pair<int,int>>> v;
+int N,M,D,S,m,p,t,sick[51],possible[51],cnt,ans,badmilk; 
+vector<vector<pair<int,int>>> v;
 bool flag,milkflag[51];
 
-void solve(){
+int main(){
+	setIO("badmilk");
 	cin >> N >> M >> D >> S;
-	v.rsz(N+1);
+	v.resize(N+1);
 	for (int i=0;i<D;++i){
 		cin >> p >> m >> t;
 		v[p].pb(mp(t,m));
@@ -131,31 +82,6 @@ void solve(){
 		}
 	}
 	ans=max(ans,cnt);
-	cout << S+ans << endl;
-}
-// you should actually read the stuff below
-// read!read!read!read!read!read!read!read!read!
-// ll vs. int!
- 
-/* stuff you should look for
-* int overflow, array bounds
-* special cases (n=1?)
-* do smth instead of nothing and stay organized
-* WRITE STUFF DOWN
-* DON'T GET STUCK ON ONE APPROACH
-*/
-int main(){
-	//string name="diamond";
-	setIO("badmilk");
-	//cout.tie(NULL);
-	/*
-	cin >> t;
-	for (int i=1;i<=t;++i){
-		//cout << "Test " << i << endl; 
-		solve();
-	}
-	*/
-	solve();
-	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC <<" secs"<<endl; 
+	cout << S+ans;
 	return 0;
 }
