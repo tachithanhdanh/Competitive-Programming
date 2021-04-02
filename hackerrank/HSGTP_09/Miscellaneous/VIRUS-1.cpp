f6844cf89c2645d05273d9f5459126e5dd81b6e9
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-04-02 09:34:31 +0700

    Add files via upload

---
+++ hackerrank/HSGTP_09/Miscellaneous/VIRUS-1.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h> // see /general/running-code-locally
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+
+//vectors
+using vi = vector<int>;
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define pb push_back
+
+//pairs
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+//for printing variables when debugging
+#ifdef LOCAL
+#define debug(x) cout << (#x) << " is " << (x) << endl
+#else
+#define debug(x)
+#endif // LOCAL
+
+//Set min-max value.
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+template<class T> bool ckmax(T& a, const T& b) { return b > a ? a = b, 1 : 0; } // set a = max(a,b)
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+	if (sz(name)) {
+		freopen((name+".in").c_str(), "r", stdin); // see /general/io
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+//constant initialization
+const string yes="YES\n",no="NO\n";
+const int MOD = 1e9+7; //998244353
+const int MX = 1e3+10;
+const ll INF = 1e18; //Not too close to LLONG_MAX
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+const int prime[] = {2, 3, 5, 7}; // prime numbers below 10.
+
+//variables used for the current problem
+ll N;
+
+ll fastpow(ll x, ll y) {
+	ll res = 1;
+	while (y) {
+		if (y&1) (res *= x) %= 123456789;
+		y >>= 1;
+		(x *= x) %= 123456789;
+	}
+	return res;
+}
+
+int main() {
+	setIO();
+	#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+	#endif // LOCAL
+	cin >> N;
+	cout << fastpow(2,N);
+	return 0;
+}
