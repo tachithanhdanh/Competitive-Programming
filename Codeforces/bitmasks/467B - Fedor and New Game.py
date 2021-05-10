887b50c9e955af08a8b0d0fb484c617f8005921d
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2021-05-10 09:14:41 +0700

    Update 467B - Fedor and New Game.py

--- Codeforces/bitmasks/467B - Fedor and New Game.py
+++ Codeforces/bitmasks/467B - Fedor and New Game.py
@@ -2,18 +2,15 @@
 # Author: Danh Ta Chi Thanh
 
 n, m, k = map(int,input().split());
-a = [];
-for i in range(m+1):
-	num = int(input());
-	a.append(num);
+a = [int(input()) for x in range(m+1)];
 ans = 0;
 
 for i in range(m):
-	bit, counts = 1, 0;
-	while (bit < (1 << 22)):
-		b1, b2 = bit & a[i], bit & a[m];
-		counts += bool(b1 ^ b2);
-		bit <<= 1;
+	x = a[i] ^ a[m];
+	counts = 0;
+	while (x):
+		counts += x & 1;
+		x >>= 1;
 	if (counts <= k):
 		ans += 1;
 
