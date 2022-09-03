66f46e16d7878aa730bbb84c1f20da5b57209f50
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2022-09-03 19:12:27 +0700

    Add debugging template (Thanks  SansPapyrus683)

---
+++ Templates/debugging.h
@@ -0,0 +1,140 @@
+/**
+ * Author: Kevin Sheng (SansPapyrus683)
+ * Date: 2021-12-12
+ * Description: Debugging Template
+ * Source: https://gist.github.com/SansPapyrus683/61b65d4d7ec223b48ebf5c3bb382ba8d
+ * Usage: Printing out frequently used STL Containers (vector, set, map, deque) and pair, tuple
+ * Status: Maybe judiciously tested (I believe in USACO Content Manager, lol!)
+ */
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {
+    if (vec.empty()) {
+        out << "[]";
+        return out;
+    }
+    out << '[';
+    for (int i = 0; i < vec.size() - 1; i++) {
+        out << vec[i] << ", ";
+    }
+    return out << vec.back() << ']';
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& pair) {
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::deque<T>& deq) {
+    if (deq.empty()) {
+        out << "[]";
+        return out;
+    }
+    out << '[';
+    for (int i = 0; i < deq.size() - 1; i++) {
+        out << deq[i] << ", ";
+    }
+    return out << deq.back() << ']';
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::unordered_map<T1, T2>& map) {
+    out << '{';
+    for (auto it = map.begin(); it != map.end(); it++) {
+        std::pair<T1, T2> element = *it;
+        out << element.first << ": " << element.second;
+        if (std::next(it) != map.end()) {
+            out << ", ";   
+        }
+    }
+    return out << '}';
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::map<T1, T2>& map) {
+    out << '{';
+    for (auto it = map.begin(); it != map.end(); it++) {
+        std::pair<T1, T2> element = *it;
+        out << element.first << ": " << element.second;
+        if (std::next(it) != map.end()) {
+            out << ", ";   
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::unordered_set<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";   
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::multiset<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";   
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::unordered_multiset<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";   
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::set<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";   
+        }
+    }
+    return out << '}';
+}
+
+// Source: https://stackoverflow.com/a/31116392/12128483
+template<typename Type, unsigned N, unsigned Last>
+struct TuplePrinter {
+    static void print(std::ostream& out, const Type& value) {
+        out << std::get<N>(value) << ", ";
+        TuplePrinter<Type, N + 1, Last>::print(out, value);
+    }
+};
+
+template<typename Type, unsigned N>
+struct TuplePrinter<Type, N, N> {
+    static void print(std::ostream& out, const Type& value) {
+        out << std::get<N>(value);
+    }
+};
+
+template<typename... Types>
+std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& value) {
+    out << '(';
+    TuplePrinter<std::tuple<Types...>, 0, sizeof...(Types) - 1>::print(out, value);
+    return out << ')';
+}
