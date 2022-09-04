9d05e2d7dbb9d29bb46f403754b74566ef960904
tachithanhdanh <75555549+tachithanhdanh@users.noreply.github.com>
2022-09-04 16:56:22 +0700

    Upload solution for Made Up

---
+++ USACO/Bronze/intro_to_sets_and_maps/made_up_atcoder.cpp
@@ -0,0 +1,74 @@
+/**
+ *    author:    tachithanhdanh
+ *    created:   2022-09-04    16:47:58
+**/
+
+#include <bits/stdc++.h> // see /general/running-code-locally
+using namespace std;
+
+#ifdef LOCAL
+#include "debugging.h"
+#endif
+
+#define endl "\n"
+
+using ll = long long;
+using str = string;
+
+// vectors
+using vi = vector<int>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<str>;
+#define all(x) begin(x), end(x)
+#define sz(x) int(size(x))
+#define pb push_back
+
+// pairs
+using pi = pair<int, int>;
+using vpi = vector<pi>;
+#define f first
+#define s second
+#define mp make_pair
+
+// set min-max value.
+template<typename T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+template<typename T> bool ckmax(T& a, const T& b) { return b > a ? a = b, 1 : 0; } // set a = max(a,b)
+
+// constant initialization
+const str yes="YES\n", no="NO\n";
+const int MOD = 1e9 + 7; // 998244353
+const int MX = 1e5 + 10;
+const ll INF = 1e18; //Not too close to LLONG_MAX
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1}; // for every grid problem!!
+const char dc[4] = {'r', 'u', 'l', 'd'};
+const bool multitest = false;
+
+void solve() {
+	int n;
+	cin >> n;
+	int a[MX], b[MX], c[MX], cntA[MX]{}, cntC[MX]{};
+	ll ans{};
+	for (int i = 0; i < n; ++i) cin >> a[i]; 
+	for (int i = 0; i < n; ++i) cin >> b[i]; 
+	for (int i = 0; i < n; ++i) cin >> c[i];
+	for (int i = 0; i < n; ++i) ++cntA[a[i]];
+	for (int i = 0; i < n; ++i) ++cntC[c[i]];
+	for (int k = 0; k < n; ++k) {
+		ans += cntA[b[k]] * cntC[k+1];
+	}
+	cout << ans;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+	#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+	#endif // LOCAL
+	int tt = 1;
+	if (multitest) cin >> tt;
+	for (int t = 0; t < tt; ++t) {
+		solve();
+	}
+	return 0;
+}
